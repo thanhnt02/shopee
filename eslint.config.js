93edf1427c7d76d3785dabb9c4e4cef458f90326
thanhnt <nguyentrithanh2409@gmail.com>
2025-10-20 08:01:03 +0700

    Setup Vite for Project

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettier from 'eslint-plugin-prettier'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'vite'
+
+export default defineConfig([
+  { ignores: ['dist'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  jsxA11y.flatConfigs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        node: {
+          paths: ['src'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      }
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettier
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'warn',
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: [
+            '^/.*\\.(svg|png|jpg|jpeg|gif|ico|webp)$',
+            '^@vitejs/',
+            '^@tailwindcss/',
+            '^vite$',
+            '^tailwindcss$',
+            '^typescript-eslint$',
+            '^eslint-plugin-',
+            '^eslint-config-',
+            '^@eslint/',
+            '^globals$'
+          ]
+        }
+      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  }
+])
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+  plugins: [react(), tailwindcss()]
+})
